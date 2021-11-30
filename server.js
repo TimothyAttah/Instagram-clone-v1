77492beece9376f918ec40dbfc2a2b389205c0ff
Timothy Attah <timothyattah733@gmail.com>
2021-11-30 17:58:27 +0100

    Testing heroku app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use( express.json() );
 app.use( cors() );
 
 app.get( '/', ( req, res ) => {
-  res.send({hello: 'world'})
+  res.send({hello: 'world. This is a test'})
 })
 
 const PORT = process.env.PORT || 5000;
