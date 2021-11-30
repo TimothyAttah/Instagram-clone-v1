e997a7ce9a103b1ee4479124dbaac518b5cbaa45
Timothy Attah <timothyattah733@gmail.com>
2021-11-30 19:48:30 +0100

    delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
+      
         <Route exact path='/'>
           <Header />
         </Route>
-      </Switch>
+    
     </BrowserRouter>
   )
 }
