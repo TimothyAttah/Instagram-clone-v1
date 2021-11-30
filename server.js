9865aeadc94652d6c6e300239ced5ea1b81b90f2
Timothy Attah <timothyattah733@gmail.com>
2021-11-30 19:12:13 +0100

    fix errors of npm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require( 'express' );
 const cors = require( 'cors' );
+const path = require('path');
 
 const app = express();
 
@@ -10,15 +11,15 @@ app.get( '/', ( req, res ) => {
   res.send({hello: 'world. This is a test!!!!'})
 })
 
-const PORT = process.env.PORT || 5000;
 
 if ( process.env.NODE_ENV === 'production' ) {
   app.use( express.static( 'client/build' ) );
-  const path = require( 'path' );
 
   app.get( '*', ( req, res ) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) );
   })
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`))
