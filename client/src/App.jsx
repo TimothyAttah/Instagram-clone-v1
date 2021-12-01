48e463f97cbad7b2e069dc6f625258c9b1f20ccf
Timothy Attah <timothyattah733@gmail.com>
2021-12-01 23:45:52 +0100

    Created history.js file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Routes, Route, BrowserRouter } from 'react-router-dom';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import { Header } from './components/Header';
+// import { history } from './history';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Header /> } />
+        <Route path='/' element={<Header />} />
     </Routes>
     </BrowserRouter>
   )
