bb9bdbfa4dffaece3242e267987f53038203d220
Timothy Attah <timothyattah733@gmail.com>
2021-12-03 20:05:19 +0100

    Added new links in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div>
       <h1>Header is here...</h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   )
 }
