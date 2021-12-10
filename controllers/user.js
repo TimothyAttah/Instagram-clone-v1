503a9d7c159d6a8f6594c62b32e0b03949e19234
Timothy Attah <timothyattah733@gmail.com>
2021-12-10 19:41:42 +0100

    Created get a user route

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,19 @@ export const userControllers = {
       res.status( 200 ).json( allUsers );
     } catch (err) {
       console.log( `Error: ${ err.message }` );
-      res.status(500).json({error: err.message})
+      res.status( 500 ).json( { error: err.message } );
+    }
+  },
+  getAUser: async ( req, res ) => {
+    const _id = req.params.userId;
+    try {
+      const user = await User.findOne({ _id}).select('-password');
+      if ( !user )
+        return res.status( 404 ).json( { error: 'This account is not yours. Access denied.' } );
+      res.status( 200 ).json( { user } );
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+			res.status(500).json({ error: err.message });
     }
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { userControllers } from "../controllers/user.js";
+import { auth } from '../middlewares/auth.js';
 
 export const userRouter = Router();
 
 userRouter.get( '/', userControllers.getAllUsers );
+
+userRouter.get( '/user/:userId', auth, userControllers.getAUser );
