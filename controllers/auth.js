fb2f9558d5f2e1b53a2b4237a95646c048d9a3f5
Timothy Attah <timothyattah733@gmail.com>
2021-12-10 20:11:21 +0100

    Created change username route

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,5 +64,38 @@ export const authControllers = {
       console.log(`Error: ${err}`);
        res.status(500).json({ error: err.message });
     }
+  },
+  changeUsername: async ( req, res ) => {
+    	const newUsername = req.body.username;
+			User.findOne({ username: req.body.username }).then(savedUser => {
+				if (savedUser) {
+					return res.status(422).json({
+						error: 'This username is already taken. Please try a different one',
+					});
+				}
+				User.findOne({ email: req.body.email })
+					.then(user => {
+						if (!user) {
+							return res.status(422).json({ error: 'Invalid Credentials' });
+						}
+						if (newUsername.length < 6) {
+							return res.status(422).json({
+								error: `Your username must be having 6 or more characters. You have entered only ${newUsername.length} characters.`,
+							});
+						}
+						if (newUsername.length > 30) {
+							return res.status(422).json({
+								error: `Your username must not be having more than 30 characters. You have entered ${newUsername.length} characters.`,
+							});
+						}
+						user.username = newUsername;
+						user.save().then(user => {
+							res.json({ message: 'Username changed successfully.' });
+						});
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			});
   }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { authControllers } from "../controllers/auth.js";
+import { auth } from '../middlewares/auth.js';
 
 export const authRouter = Router();
 
 authRouter.post( '/signup', authControllers.signupUser );
 
 authRouter.post( '/signin', authControllers.signinUser );
+
+authRouter.post('/change-username', auth, authControllers.changeUsername);
