6a48e975a9641096b4c2aaf7ff64139880bd00f7
Timothy Attah <timothyattah733@gmail.com>
2021-12-12 16:09:05 +0100

    Added and created upload file route

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-// const express = require( 'express' );
-// const cors = require( 'cors' );
-// const path = require( 'path' );
-
 import express from 'express';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 import path from 'path';
 import { connectDB } from './config/db.js';
 import { authRouter } from './routes/auth.js';
@@ -15,6 +12,10 @@ connectDB();
 
 app.use( express.json() );
 app.use( cors() );
+app.use( express.json( { limit: '50mb' } ) );
+app.use( express.urlencoded( { limit: '50mb', extended: true } ) );
+app.use( fileUpload() );
+
 
 app.get( '/', ( req, res ) => {
   res.send({hello: 'world. This is a test!!!!'})
@@ -23,6 +24,33 @@ app.get( '/', ( req, res ) => {
 app.use( '/api/auth', authRouter )
 app.use( '/api/users', userRouter );
 app.use( '/api/posts', postRouter );
+
+
+app.post('/api/upload', (req, res) => {
+	if (req.files === null) {
+		res.status(200).json({ msg: 'No file uploaded' });
+	}
+	const file = req.files.file;
+	const newFile = `${Date.now()}_${file.name}`;
+
+	// file.mv(`${__dirname}/frontend/src/uploads/${newFile}`, err => {
+	// 		if (err) {
+	// 			console.error(err);
+	// 			return res.status(500).send(err);
+	// 		}
+	// 		res.json({ fileName: newFile, filePath: `/uploads/${newFile}` });
+	// 	});
+
+	file.mv(`${__dirname}/client/public/uploads/${newFile}`, err => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send(err);
+		}
+
+		res.json({ fileName: newFile, filePath: `/uploads/${newFile}` });
+	});
+});
+
 
 
 if ( process.env.NODE_ENV === 'production' ) {
