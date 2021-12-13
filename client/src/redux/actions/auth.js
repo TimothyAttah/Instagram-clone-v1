f5c5afbb4c5a4da2627a8b093e96df8450d01486
Timothy Attah <timothyattah733@gmail.com>
2021-12-13 16:11:21 +0100

    Created and dispatched signin user action

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -17,3 +17,22 @@ export const signupUser = (userData) => async dispatch => {
     }
   }
 }
+
+export const signinUser = (userData) => async dispatch => {
+  try {
+    const { data } = await api.signinUser(userData);
+		dispatch({
+			type: authTypes.SIGN_IN,
+			payload: data.user,
+    } );
+    console.log( 'Sign in user data >>>>', data );
+    localStorage.setItem( 'jwt', data.token );
+    localStorage.setItem( 'user', JSON.stringify( data.user ) );
+    toast( data.message );
+    window.location.href = '/';
+  } catch (err) {
+    if ( err.response && err.response.data ) {
+      toast( err.response.data.error );
+    }
+  }
+}
