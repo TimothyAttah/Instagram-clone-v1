45e659916631c2b9649ce23bc7cc21f9de2ba08f
Timothy Attah <timothyattah733@gmail.com>
2021-12-22 04:51:29 +0100

    Created mongo db cluster

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 const keys = {
-	MONGODB_URL: 'mongodb://localhost:27017/instagram-clone-v2',
+	MONGODB_URL:
+		'mongodb+srv://timothy1234:timo1234@cluster0.qlltw.mongodb.net/instagram-clone?retryWrites=true&w=majority',
+	// MONGODB_URL: 'mongodb://localhost:27017/instagram-clone-v2',
 	JWT_SECRET: 'a1b2c3d4e5rffdrtseduhjj5@k_jhl#hgy',
 };
 
